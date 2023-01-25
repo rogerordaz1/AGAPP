e9103bdd3d6b2f33501a38b80365a094c50c90f7
Roger Ordaz <rogerordaz98@gmail.com>
2023-01-24 21:39:39 -0800

    new

---
+++ resources/views/panel_admin.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.prueba')
+{{-- @section('title', 'Roger') --}}
+@section('content')
+{{-- <li class="nav-item d-none d-sm-inline-block">
+  <a href="#" class="nav-link">Item desde la otra Pag</a>
+</li>   --}}
+@parent
+<div>
+
+  <h1>Hello XDXDXD</h1>
+</div>
+@endsection
